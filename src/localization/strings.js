cfcce383f856b35f1a54c9c517f716c50d9200d4
Patrikoh <patrik.olsson@axakon.se>
2023-01-11 14:48:35 +0100

    WIP

--- src/localization/strings.js
+++ src/localization/strings.js
@@ -1,137 +1,156 @@
-
 const strings = {
-    swedish: {
-        header: {
-            rsvp: "OSA",
-            ourStory: "Vår historia",
-            information: "Information",
-            logOut: "Logga ut",
-        },
-        addGuests: {
-            header: 'Anmäl gäster',
-            firstName: 'Förnamn',
-            lastName: 'Efternamn',
-            foodPreferences: 'Matpreferenser',
-            attendingDates: 'Deltagande',
-            attendingDatesPlaceholder: 'Välj datum...',
-            takesBussPlaceholder: "Välj...",
-            fridayToSunday: 'Fredag - söndag (15/8 - 17/8)',
-            saturdayToSunday: 'Lördag - söndag (16/8 - 17/8)',
-            yes: 'Ja',
-            no: 'Nej',
-            other: 'Övrigt',
-            addGuest: 'Lägg till gäst',
-            takesBuss: 'Önskar ta buss från Stockholm'
-        },
-        ourStory: {
-            header: 'Vår historia',
-            story: [
-                {
-                    title: '20 aug 2021',
-                    text: 'Its a match! Pauline har ställt in tinders radie på två km för att hon inte orkar träffa någon' +
-                        'som bor allt för långt bort. Patrik hälsar på en kompis i Aspudden och lyckas matcha med Pauline innan han' +
-                        'beger sig hemåt till Åkersberga 38km bort. Efter mycket swipande blev det en match!'
-                },
-                {
-                    title: 'Onsdag 25 augusti',
-                    text: '"Ses på en kaffe idag?" Första dejten blev en spontan kaffe to-go under ett träd i' +
-                    'Vasaparken. Det var en lyckad dejt som ledde till ytterligare dejter både på fredagen och söndagen.'
-                },
-                {
-                    title: '20 aug 2021',
-                    text: 'Its a match! Pauline har ställt in tinders radie på två km för att hon inte orkar träffa någon' +
-                        'som bor allt för långt bort. Patrik hälsar på en kompis i Aspudden och lyckas matcha med Pauline innan han' +
-                        'beger sig hemåt till Åkersberga 38km bort. Efter mycket swipande blev det en match!'
-                },
-                {
-                    title: 'Onsdag 25 augusti',
-                    text: '"Ses på en kaffe idag?" Första dejten blev en spontan kaffe to-go under ett träd i' +
-                        'Vasaparken. Det var en lyckad dejt som ledde till ytterligare dejter både på fredagen och söndagen.'
-                }
-            ]
-        },
-        information: {
-            header: 'Information',
-            staying: {
-                title: 'Boende',
-                text: 'Ni väljer om ni vill stanna en natt eller två på Lidö. Här bor man antingen intill värdshuset där festen är\n' +
-                    'eller i gäststugor 7 min från värdshuset. Vi kommer att boka boende till er när ni anmält vilka dagar ni\n' +
-                    'närvarar. En natt kostar kring 700 kr/person och 2 nätter 1400 kr. Dessa priser kan komma att justeras något då värdshuset inte satt de slutgiltiga priserna för året. Frukostbuffé, städning, sänglinne och handdukar\n' +
-                    'ingår. Det går att hyra spjälsäng.',
-            },
-            location: {
-              title: 'Plats',
-              text: 'Bröllopet kommer att vara på Lidö värdshus, på den vackra skärgårdsön Lidö en bit utanför Norrtälje. Hit tar man sig lättast med bil till Räfsnäs brygga och därefter passbåt till Lidö. En buss till Räfsnäs brygga kommer att ordnas ifrån Stockholm för de som vill.',
-            },
-            children: {
-                title: 'Barn',
-                text: 'Denna dag firar vi gärna med våra vuxna vänner och släktningar. Ammande barn är givetvis välkomna.',
-            },
-            clothes: {
-                title: 'Klädsel',
-                text: 'Klädkod på bröllopet är mörk kostym.',
-            },
-            gifts: {
-                title: 'Presenter',
-                text: 'Den största gåvan vi kan önska är att ni närvarar under vår bröllopshelg. Önskar ni ändå ge någonting så får ni gärna skicka ett biddrag till hjärt och lungfonden - som är en hjälporganisation som står oss nära.',
-            },
-            toastMaster: {
-                title: 'Toastmaster',
-                text: 'Patriks bror Jonas kommer att vara toastmaster på bröllopet. Om ni önskar hålla tal eller framföra något annat - väligen kontakta Jonas.',
-            },
-        }
+  swedish: {
+    header: {
+      rsvp: "OSA",
+      ourStory: "Vår historia",
+      information: "Information",
+      logOut: "Logga ut",
     },
-    english: {
-        header: {
-            rsvp: "RSVP",
-            ourStory: "Our story",
-            information: "Information",
-            logOut: "Log out",
+    addGuests: {
+      header: "Anmäl gäster",
+      firstName: "Förnamn",
+      lastName: "Efternamn",
+      foodPreferences: "Matpreferenser",
+      attendingDates: "Deltagande",
+      attendingDatesPlaceholder: "Välj datum...",
+      takesBussPlaceholder: "Välj...",
+      fridayToSunday: "Fredag - söndag (15/8 - 17/8)",
+      saturdayToSunday: "Lördag - söndag (16/8 - 17/8)",
+      yes: "Ja",
+      no: "Nej",
+      other: "Övrigt",
+      addGuest: "Lägg till gäst",
+      takesBuss: "Önskar ta buss från Stockholm",
+    },
+    ourStory: {
+      header: "Vår historia",
+      story: [
+        {
+          title: "20 aug 2021",
+          text:
+            "Its a match! Pauline har ställt in Tinders radie på två km för att hon inte orkar träffa någon " +
+            "som bor allt för långt bort. Patrik hälsar på en kompis i Aspudden och matchar med Pauline innan han " +
+            "beger sig hemåt till Åkersberga 38km bort. Efter mycket swipande blev det en match!",
         },
-        addGuests: {
-            header: 'Add guests',
-            firstName: 'First name',
-            lastName: 'Last name',
-            foodPreferences: 'Food preferences',
-            attendingDates: 'Attending dates',
-            attendingDatesPlaceholder: 'Choose dates...',
-            fridayToSunday: 'Friday - sunday (15/8 - 17/8)',
-            saturdayToSunday: 'Saturday - sunday (16/8 - 17/8)',
-            other: 'Other information',
-            addGuest: 'Add guest',
+        {
+          title: "Onsdag 25 augusti",
+          text:
+            '"Ses på en kaffe idag?" Första dejten blev en spontan kaffe to-go under ett träd i ' +
+            "Vasaparken. Det var en lyckad dejt som ledde till ytterligare dejter både på fredagen och söndagen.",
         },
-        ourStory: {
-            header: 'Our story',
-            story: [
-                {
-                    title: '20 aug 2021',
-                    text: 'Its a match! Pauline har ställt in tinders radie på två km för att hon inte orkar träffa någon' +
-                        'som bor allt för långt bort. Patrik hälsar på en kompis i Aspudden och lyckas matcha med Pauline innan han' +
-                        'beger sig hemåt till Åkersberga 38km bort. Efter mycket swipande blev det en match!'
-                },
-                {
-                    title: 'Onsdag 25 augusti',
-                    text: '"Ses på en kaffe idag?" Första dejten blev en spontan kaffe to-go under ett träd i' +
-                        'Vasaparken. Det var en lyckad dejt som ledde till ytterligare dejter både på fredagen och söndagen.'
-                },
-                {
-                    title: '20 aug 2021',
-                    text: 'Its a match! Pauline har ställt in tinders radie på två km för att hon inte orkar träffa någon' +
-                        'som bor allt för långt bort. Patrik hälsar på en kompis i Aspudden och lyckas matcha med Pauline innan han' +
-                        'beger sig hemåt till Åkersberga 38km bort. Efter mycket swipande blev det en match!'
-                },
-                {
-                    title: 'Onsdag 25 augusti',
-                    text: '"Ses på en kaffe idag?" Första dejten blev en spontan kaffe to-go under ett träd i' +
-                        'Vasaparken. Det var en lyckad dejt som ledde till ytterligare dejter både på fredagen och söndagen.'
-                }
-            ]
+        {
+          title: "20 aug 2021",
+          text: "I samband med att Pauline hjälper Patrik att installera en ny spis skadar sig Pauline på fingret och får en otäck infektion. Efter att tillsammans ha gått igenom proccessen som operation och vistelse på akuten innebar konsterade Pauline och Patrik att dom nog var ett par.",
         },
-        information: {
-            header: 'Information',
-            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis euismod sagittis porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur congue nisl nec nulla porta bibendum. Sed cursus finibus lorem eget aliquam. Phasellus sagittis orci auctor semper interdum. Proin et diam leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam et tempus dolor, vel feugiat erat.",
-        }
-    }
+        {
+          title: "Onsdag 25 augusti",
+          text: "Patrik och Pauline bestämmer sig för att åka till Lissabon tillsammans. En väldbehövd resa efter allt strul med finger-infektioner.",
+        },
+        {
+          title: "Onsdag 25 augusti",
+          text: "Pauline bjuder Patrik på en weekend i Mariestad. Beslutet om att gifta sig är nu taget och tanken var att Mariestad kunde vara en möjlig plats för detta. På kvällen efter en romantisk middag beställer Patrik och Pauline ringarna.",
+        },
+        {
+          title: "Onsdag 25 augusti",
+          text: "Ringarna anländer på posten och Partik och Pauline friar till varandra utanför ICA Kvantum i Åkersberga.",
+        },
+        {
+          title: "Onsdag 25 augusti",
+          text: "Patrik och Pauline bestämmer sig för att försöka skaffa barn. En vecka efter beslutet är Pauline gravid.",
+        },
+        {
+          title: "Onsdag 25 augusti",
+          text: "Lägenheterna är sålda och efter att ha mellanlandat i Hornstull och sedan Åkersberga flyttar Patrik och Pauline in tillsammans på Drejargatan 8.",
+        },
+      ],
+    },
+    information: {
+      header: "Information",
+      staying: {
+        title: "Boende",
+        text:
+          "Ni väljer om ni vill stanna en natt eller två på Lidö. Här bor man antingen intill värdshuset där festen är\n" +
+          "eller i gäststugor 7 min från värdshuset. Vi kommer att boka boende till er när ni anmält vilka dagar ni\n" +
+          "närvarar. En natt kostar kring 700 kr/person och 2 nätter 1400 kr. Dessa priser kan komma att justeras något då värdshuset inte satt de slutgiltiga priserna för året. Frukostbuffé, städning, sänglinne och handdukar\n" +
+          "ingår. Det går att hyra spjälsäng.",
+      },
+      location: {
+        title: "Plats",
+        text: "Bröllopet kommer att vara på Lidö värdshus, på den vackra skärgårdsön Lidö en bit utanför Norrtälje. Hit tar man sig lättast med bil till Räfsnäs brygga och därefter passbåt till Lidö. En buss till Räfsnäs brygga kommer att ordnas ifrån Stockholm för de som vill.",
+      },
+      children: {
+        title: "Barn",
+        text: "Denna dag firar vi gärna med våra vuxna vänner och släktningar. Ammande barn är givetvis välkomna.",
+      },
+      clothes: {
+        title: "Klädsel",
+        text: "Klädkod på bröllopet är mörk kostym.",
+      },
+      gifts: {
+        title: "Presenter",
+        text: 'Den största gåvan vi kan önska är att ni närvarar under vår bröllopshelg. Önskar ni ändå ge någonting så får ni gärna skicka ett biddrag till <a href="https://www.hjart-lungfonden.se/stod-oss/gava/">hjärt of lungfonden</a> - som är en hjälporganisation som står oss nära.',
+      },
+      toastMaster: {
+        title: "Toastmaster",
+        text: "Patriks bror Jonas kommer att vara toastmaster på bröllopet. Om ni önskar hålla tal eller framföra något annat - vänligen kontakta Jonas.",
+      },
+    },
+  },
+  english: {
+    header: {
+      rsvp: "RSVP",
+      ourStory: "Our story",
+      information: "Information",
+      logOut: "Log out",
+    },
+    addGuests: {
+      header: "Add guests",
+      firstName: "First name",
+      lastName: "Last name",
+      foodPreferences: "Food preferences",
+      attendingDates: "Attending dates",
+      attendingDatesPlaceholder: "Choose dates...",
+      fridayToSunday: "Friday - sunday (15/8 - 17/8)",
+      saturdayToSunday: "Saturday - sunday (16/8 - 17/8)",
+      other: "Other information",
+      addGuest: "Add guest",
+    },
+    ourStory: {
+      header: "Our story",
+      story: [
+        {
+          title: "20 aug 2021",
+          text:
+            "Its a match! Pauline har ställt in tinders radie på två km för att hon inte orkar träffa någon" +
+            "som bor allt för långt bort. Patrik hälsar på en kompis i Aspudden och lyckas matcha med Pauline innan han" +
+            "beger sig hemåt till Åkersberga 38km bort. Efter mycket swipande blev det en match!",
+        },
+        {
+          title: "Onsdag 25 augusti",
+          text:
+            '"Ses på en kaffe idag?" Första dejten blev en spontan kaffe to-go under ett träd i' +
+            "Vasaparken. Det var en lyckad dejt som ledde till ytterligare dejter både på fredagen och söndagen.",
+        },
+        {
+          title: "20 aug 2021",
+          text:
+            "Its a match! Pauline har ställt in tinders radie på två km för att hon inte orkar träffa någon" +
+            "som bor allt för långt bort. Patrik hälsar på en kompis i Aspudden och lyckas matcha med Pauline innan han" +
+            "beger sig hemåt till Åkersberga 38km bort. Efter mycket swipande blev det en match!",
+        },
+        {
+          title: "Onsdag 25 augusti",
+          text:
+            '"Ses på en kaffe idag?" Första dejten blev en spontan kaffe to-go under ett träd i' +
+            "Vasaparken. Det var en lyckad dejt som ledde till ytterligare dejter både på fredagen och söndagen.",
+        },
+      ],
+    },
+    information: {
+      header: "Information",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis euismod sagittis porta. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur congue nisl nec nulla porta bibendum. Sed cursus finibus lorem eget aliquam. Phasellus sagittis orci auctor semper interdum. Proin et diam leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam et tempus dolor, vel feugiat erat.",
+    },
+  },
 };
 
 export default strings;
--- src/views/Home/OurStory/OurStory.jsx
+++ src/views/Home/OurStory/OurStory.jsx
@@ -1,87 +1,110 @@
-import React, { useState } from 'react';
-import styles from './OurStory.module.scss';
+import React, { useState } from "react";
+import styles from "./OurStory.module.scss";
 import { useSelector } from "react-redux";
 import strings from "../../../localization/strings";
-import vasaparken from '../../../images/vasaparken.jpg';
-import { BrowserView, MobileView, isMobile } from 'react-device-detect';
+import aspudden from "../../../images/aspudden.jpeg";
+import vasaparken from "../../../images/vasaparken.jpg";
+import mariestad from "../../../images/mariestad.jpeg";
+import danderyds from "../../../images/danderyds.jpeg";
+import lissabon from "../../../images/lissabon.jpg";
+import bebis from "../../../images/bebis.jpg";
+import birkastan from "../../../images/birkastan.jpeg";
+import { BrowserView, MobileView, isMobile } from "react-device-detect";
 import StoryCard from "./StoryCard/StoryCard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faChevronLeft, faChevronRight} from "@fortawesome/free-solid-svg-icons";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 import SectionHeader from "../../../common/SectionHeader/SectionHeader";
 
 const storyImages = [
-    vasaparken,
-    vasaparken,
-    vasaparken,
-    vasaparken,
+  aspudden,
+  vasaparken,
+  danderyds,
+  lissabon,
+  mariestad,
+  vasaparken,
+  bebis,
+  birkastan,
 ];
 
 const MAX_LEFT = 40;
-const MIN_LEFT = -980;
+const MIN_LEFT = -2100;
 
-const OurStory = ({pageRef}) => {
-    const [left, setLeft] = useState(isMobile ? 0 : 40);
-    const [selectedCardId, setSelectedCardId] = useState(0);
-    const language = useSelector(state => state.language);
-    const { ourStory } = strings[language];
+const OurStory = ({ pageRef }) => {
+  const [left, setLeft] = useState(isMobile ? 0 : 40);
+  const [selectedCardId, setSelectedCardId] = useState(0);
+  const language = useSelector((state) => state.language);
+  const { ourStory } = strings[language];
 
-    const selectedCard = ourStory?.story?.[selectedCardId];
+  const selectedCard = ourStory?.story?.[selectedCardId];
 
-    const scrollLeft = () => {
-        setLeft(oldLeft => oldLeft + 340);
-        if (selectedCardId > 0) {
-            setSelectedCardId(oldCard => oldCard - 1);
-        }
-    };
+  const scrollLeft = () => {
+    setLeft((oldLeft) => oldLeft + 340);
+    if (selectedCardId > 0) {
+      setSelectedCardId((oldCard) => oldCard - 1);
+    }
+  };
 
-    const scrollRight = () => {
-        setLeft(oldLeft => oldLeft - 340);
-        if (selectedCardId < ourStory.story.length - 1) {
-            setSelectedCardId(oldCard => oldCard + 1);
-        }
-    };
+  const scrollRight = () => {
+    setLeft((oldLeft) => oldLeft - 340);
+    if (selectedCardId < ourStory.story.length - 1) {
+      setSelectedCardId((oldCard) => oldCard + 1);
+    }
+  };
 
-    return (
-        <div className={styles.container}>
-            <SectionHeader pageRef={pageRef} text={ourStory.header}/>
-            <MobileView>
-                <div>
-                    {ourStory.story.map((story, i) => (
-                        <div className={styles.mobileCard} key={i}>
-                            <StoryCard {...story} image={storyImages[i]} isSelected />
-                            <div className={styles.text}>
-                                <span>{story.text}</span>
-                            </div>
-                        </div>
-                        )
-                    )}
-                </div>
-            </MobileView>
-            <div className={styles.stripe}>
-                <BrowserView>
-                    <button
-                        className={styles.leftButton}
-                        onClick={scrollLeft}
-                        disabled={left >= MAX_LEFT}
-                    >
-                        <FontAwesomeIcon icon={faChevronLeft} className={styles.icon} />
-                    </button>
-                    <div className={styles.content} style={{left}}>
-                        {ourStory.story.map((story, i) => (
-                                <StoryCard {...story} isSelected={selectedCardId === i} image={storyImages[i]} onClick={() => setSelectedCardId(i)} key={i} />
-                            )
-                        )}
-                    </div>
-                    <button className={styles.rightButton} onClick={scrollRight} disabled={left <= MIN_LEFT}><FontAwesomeIcon icon={faChevronRight} className={styles.icon} /></button>
-                </BrowserView>
+  return (
+    <div className={styles.container}>
+      <SectionHeader pageRef={pageRef} text={ourStory.header} />
+      <MobileView>
+        <div>
+          {ourStory.story.map((story, i) => (
+            <div className={styles.mobileCard} key={i}>
+              <StoryCard {...story} image={storyImages[i]} isSelected />
+              <div className={styles.text}>
+                <span>{story.text}</span>
+              </div>
             </div>
-            <BrowserView>
-                <div className={styles.text}>
-                    <span>{selectedCard.text}</span>
-                </div>
-            </BrowserView>
+          ))}
         </div>
-    )
+      </MobileView>
+      <div className={styles.stripe}>
+        <BrowserView>
+          <button
+            className={styles.leftButton}
+            onClick={scrollLeft}
+            disabled={left >= MAX_LEFT}
+          >
+            <FontAwesomeIcon icon={faChevronLeft} className={styles.icon} />
+          </button>
+          <div className={styles.content} style={{ left }}>
+            {ourStory.story.map((story, i) => (
+              <StoryCard
+                {...story}
+                isSelected={selectedCardId === i}
+                image={storyImages[i]}
+                onClick={() => setSelectedCardId(i)}
+                key={i}
+              />
+            ))}
+          </div>
+          <button
+            className={styles.rightButton}
+            onClick={scrollRight}
+            disabled={left <= MIN_LEFT}
+          >
+            <FontAwesomeIcon icon={faChevronRight} className={styles.icon} />
+          </button>
+        </BrowserView>
+      </div>
+      <BrowserView>
+        <div className={styles.text}>
+          <span>{selectedCard.text}</span>
+        </div>
+      </BrowserView>
+    </div>
+  );
 };
 
 export default OurStory;
